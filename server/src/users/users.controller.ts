5552083404828b6b382cd86a9adc0f56e526ca6a
Navfront <b3rt@yandex.ru>
2022-08-14 19:15:23 +0300

    clear

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,41 +1,10 @@
-import {
-  Body,
-  Controller,
-  Post,
-  Request,
-  UseGuards,
-  HttpException,
-  HttpStatus,
-  UsePipes,
-  Get,
-} from '@nestjs/common';
-import { GetUserByLogin } from './dto/get-user-by-login.dto';
+import { Body, Controller, Post, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
-// import { CreateUserDto } from './dto/create-user.dto';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
-import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
-import { ValidationPipe } from './../pipes/validation.pipe';
-import { CreateUserDto } from './dto/create-user.dto';
-import { CreateUserMDBDto } from './dto/create-user-mdb.dto';
 import { CreateClickDto } from './dto/create-click-mdb.dto';
 
-@ApiTags('Users')
 @Controller('users')
 export class UsersController {
   constructor(private userService: UsersService) {}
-
-  @ApiOperation({ summary: 'Получение юзера по ид' })
-  @UseGuards(JwtAuthGuard)
-  @Post('/')
-  getUserByLogin(@Body() dto: GetUserByLogin, @Request() req) {
-    if (dto.login !== req.user.username) {
-      throw new HttpException(
-        'Зачем тебе другие пользователи?',
-        HttpStatus.BAD_REQUEST,
-      );
-    }
-    return this.userService.getUserByLogin(dto.login);
-  }
 
   @Post('/click')
   createClick(@Body() dto: CreateClickDto) {
@@ -46,11 +15,4 @@ export class UsersController {
   getClicks() {
     return this.userService.getClicks();
   }
-
-  // @ApiOperation({ summary: 'Создание нового юзера' })
-  // @UseGuards(JwtAuthGuard)
-  // @Post('/create')
-  // createUser(@Body() dto: CreateUserDto) {
-  //   return this.userService.createUser(dto);
-  // }
 }
