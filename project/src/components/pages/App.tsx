5c7ced89c7bd9db7e0658078b919ddb456a9f4d3
Navfront <b3rt@yandex.ru>
2022-08-19 18:05:15 +0300

    types fix

--- project/src/components/pages/App.tsx
+++ project/src/components/pages/App.tsx
@@ -1,59 +1,55 @@
-import React, { useEffect, useState } from "react";
-import logo from "../../logo.svg";
-import "../../App.css";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from 'react'
+import logo from '../../logo.svg'
+import '../../App.css'
+import { Link } from 'react-router-dom'
 
+const URL = process.env.REACT_APP_SURV ?? 'https://sixcities.navfront.ru/api/'
+console.log('env react url = ', process.env.REACT_APP_SURV)
 
-const URL = process.env.REACT_APP_SURV || "https://sixcities.navfront.ru/api/"
-console.log('env react url = ', process.env.REACT_APP_SURV);
-
-
-async function getClicks() {
-  const res = await fetch(`${URL}users/clicks`);
-  const clicks = await res.json();
-  return clicks;
+async function getClicks (): Promise<Click[]> {
+  const res = await fetch(`${URL}users/clicks`)
+  const clicks = await res.json()
+  return clicks
 }
 
-type Click = {
+interface Click {
   _id: string
   clickDate: string
 }
 
-function App() {
-  const [clicks, setClicks] = useState<Click[]>([]);
+function App (): JSX.Element {
+  const [clicks, setClicks] = useState<Click[]>([])
 
   useEffect(() => {
-    getClicks().then((r) => {
-      setClicks([...clicks, ...r]);
-    });
-  }, [setClicks]);
+    void getClicks().then((r) => {
+      setClicks([...clicks, ...r])
+    })
+  }, [setClicks])
 
-  const clickHandler = () => {
+  const clickHandler = (): void => {
     fetch(`${URL}users/click`, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        'content-type': 'application/json;charset=UTF-8',
+        'content-type': 'application/json;charset=UTF-8'
       },
-      body: JSON.stringify({ clickDate: Date.now() }),
-    }).then(res => res.json()).then(data => { console.log(data);
-     setClicks([...clicks, data]) }).catch(e=>console.log(e)
-    );
-
-
-
-  };
+      body: JSON.stringify({ clickDate: Date.now() })
+    }).then(async res => await res.json()).then(data => {
+      console.log(data)
+      setClicks([...clicks, data])
+    }).catch(e => console.log(e)
+    )
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <Link to="/"><img src={logo} className="App-logo" alt="logo" /></Link>
         <h1>My db knows {clicks.length} clicks!</h1>
-        <p style={{color: "lightblue"}}>Last clicks:</p>
+        <p style={{ color: 'lightblue' }}>Last clicks:</p>
         {clicks.slice(clicks.length - 5, clicks.length).map((cl) => {
-
           const dateText = new Date(Number(cl.clickDate))
           return <p key={cl._id}>{String(dateText.toLocaleString()) }</p>
-})}
+        })}
         <a
           className="App-link"
           href="#"
@@ -64,7 +60,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
